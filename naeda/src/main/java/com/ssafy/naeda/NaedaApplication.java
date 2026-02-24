1a95633cb71eeccfac980b22b01581cbee67b788
김도영 <ehehkwat1@naver.com>
2026-02-24 15:26:21 +0900

    [BE]Init: initialized

---
+++ naeda/src/main/java/com/ssafy/naeda/NaedaApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.naeda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaedaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaedaApplication.class, args);
+	}
+
+}
---
+++ naeda/src/test/java/com/ssafy/naeda/NaedaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.naeda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaedaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
