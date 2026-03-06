962d2cfbd874a8c585db9032e5e2537bfd3a69ee
김민재 <rlaalswo0715@naver.com>
2026-03-06 10:15:42 +0900

    [BE]feat: Account DTO 추가

---
+++ naeda/src/main/java/com/ssafy/naeda/domain/account/dto/ssafy/SsafyAccountListResponse.java
@@ -0,0 +1,36 @@
+package com.ssafy.naeda.domain.account.dto.ssafy;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class SsafyAccountListResponse {
+
+    @JsonProperty("REC")
+    private List<SsafyAccountRec> rec;
+
+    @Getter
+    @NoArgsConstructor
+    public static class SsafyAccountRec {
+
+        private String bankCode;
+        private String bankName;
+        private String userName;
+        private String accountNo;
+        private String accountName;
+        private String accountTypeCode;
+        private String accountTypeName;
+        private String accountCreateDate;
+        private String accountExpiryDate;
+        private String dailyTransferLimit;
+        private String oneTimeTransferLimit;
+        private String accountBalance;
+        private String lastTransactionDate;
+        private String currency;
+
+    }
+}
