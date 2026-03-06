79cfde7eb56056b259e44bb06e4d229fcf924bad
김민재 <rlaalswo0715@naver.com>
2026-03-06 12:28:31 +0900

    [BE]fix: JwtTokenProvider import 수정 및 테스트 DB 설정 분리

--- naeda/src/main/java/com/ssafy/naeda/global/security/JwtTokenProvider.java
+++ naeda/src/main/java/com/ssafy/naeda/global/security/JwtTokenProvider.java
@@ -1,7 +1,7 @@
 package com.ssafy.naeda.global.security;
 
 
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
