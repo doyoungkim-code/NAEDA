be542f8d1751e7127fb091588a6284bd31fcf9a4
김민재 <rlaalswo0715@naver.com>
2026-03-17 10:48:03 +0900

    [BE] feat: PaymentLimit Repository 및 DTO 생성

--- naeda/src/main/java/com/ssafy/naeda/domain/payment/dto/response/PaymentLimitResponse.java
+++ naeda/src/main/java/com/ssafy/naeda/domain/payment/dto/response/PaymentLimitResponse.java
@@ -1,19 +1,24 @@
 package com.ssafy.naeda.domain.payment.dto.response;
 
 import com.ssafy.naeda.domain.payment.entity.PaymentLimit;
+import lombok.Builder;
+import lombok.Getter;
 
-public record PaymentLimitResponse(
-        Long userNo,
-        Long dailyLimit,
-        Long monthlyLimit,
-        Long singleTransactionLimit
-) {
-    public static PaymentLimitResponse from(PaymentLimit paymentLimit) {
-        return new PaymentLimitResponse(
-                paymentLimit.getUserNo(),
-                paymentLimit.getDailyLimit(),
-                paymentLimit.getMonthlyLimit(),
-                paymentLimit.getSingleTransactionLimit()
-        );
+@Getter
+@Builder
+public class PaymentLimitResponse {
+
+    private Long userNo;
+    private Long dailyLimit;
+    private Long monthlyLimit;
+    private Long singleTransactionLimit;
+
+    public static PaymentLimitResponse from (PaymentLimit paymentLimit) {
+        return PaymentLimitResponse.builder()
+                .userNo(paymentLimit.getUserNo())
+                .dailyLimit(paymentLimit.getDailyLimit())
+                .monthlyLimit(paymentLimit.getMonthlyLimit())
+                .singleTransactionLimit(paymentLimit.getSingleTransactionLimit())
+                .build();
     }
 }
