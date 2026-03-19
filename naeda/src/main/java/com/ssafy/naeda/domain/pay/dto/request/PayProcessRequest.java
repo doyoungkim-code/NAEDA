33c16c4af037faa7ab9c9f28bb3a9b720b5a5f37
kihyuk5566 <kihyuk5566@naver.com>
2026-03-19 16:34:01 +0900

    [BE]fix : fix

--- naeda/src/main/java/com/ssafy/naeda/domain/pay/dto/request/PayProcessRequest.java
+++ naeda/src/main/java/com/ssafy/naeda/domain/pay/dto/request/PayProcessRequest.java
@@ -1,12 +1,17 @@
 package com.ssafy.naeda.domain.pay.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class PayProcessRequest {
-    private String idempotencyKey;
-    private String pin;          // 2차인증 PIN (6자리, 선택)
-}
+    @NotBlank
+    private String userId;           // 얼굴 인식으로 확정된 사용자 ID
 
+    @NotBlank
+    private String idempotencyKey;
+
+    private String pin;              // 2차인증 PIN (6자리, 선택)
+}
