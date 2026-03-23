4122d9ba262b0e30e292595a99ee44dc067a7a4c
성경훈 <skh1205@naver.com>
2026-03-23 17:25:48 +0900

    [BE] fix: store 데이터 보강

--- naeda/src/test/java/com/ssafy/naeda/domain/store/bootstrap/NaverStoreEnrichmentClientTest.java
+++ naeda/src/test/java/com/ssafy/naeda/domain/store/bootstrap/NaverStoreEnrichmentClientTest.java
@@ -33,4 +33,41 @@ class NaverStoreEnrichmentClientTest {
         assertThat(client.extractPlaceImageUrl(null)).isNull();
         assertThat(client.extractPlaceImageUrl("")).isNull();
     }
+
+    @Test
+    @DisplayName("네이버 검색 결과 HTML에서 첫 place 후보를 추출한다")
+    void extractSearchNaverPlaceCandidate_extractsFirstPlaceCandidate() {
+        String html = """
+                <html>
+                  <body>
+                    <div id="place-main-section-root">
+                      <section class="place_pcnx_detail">
+                        <div id="_title">
+                          <a href="https://map.naver.com/p/entry/place/1782635585?placePath=%2Fhome">
+                            <span>김치찜은 못참지 인동점</span>
+                            <span>한식</span>
+                          </a>
+                        </div>
+                        <div class="XtBbS">오감만족 세트로 입맛 사로잡는 맛집</div>
+                        <a
+                          data-line-description="경북 구미시 인동중앙로3길 29 영무메트로 107,108호"
+                          data-kakaotalk-image-url="https://search.pstatic.net/common/?autoRotate=true&amp;quality=100&amp;type=f640_380&amp;src=https%3A%2F%2Fldb-phinf.pstatic.net%2Fkimchi.jpg">
+                        </a>
+                      </section>
+                    </div>
+                  </body>
+                </html>
+                """;
+
+        NaverStoreEnrichmentClient.SearchNaverPlaceCandidate candidate = client.extractSearchNaverPlaceCandidate(html);
+
+        assertThat(candidate).isNotNull();
+        assertThat(candidate.placeId()).isEqualTo("1782635585");
+        assertThat(candidate.name()).isEqualTo("김치찜은 못참지 인동점");
+        assertThat(candidate.category()).isEqualTo("한식");
+        assertThat(candidate.roadAddress()).isEqualTo("경북 구미시 인동중앙로3길 29 영무메트로 107,108호");
+        assertThat(candidate.imageUrl())
+                .isEqualTo("https://search.pstatic.net/common/?autoRotate=true&quality=100&type=f640_380&src=https%3A%2F%2Fldb-phinf.pstatic.net%2Fkimchi.jpg");
+        assertThat(candidate.description()).isEqualTo("오감만족 세트로 입맛 사로잡는 맛집");
+    }
 }
