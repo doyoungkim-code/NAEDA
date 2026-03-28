a1b2653e1e3f974a3b66295ab426101f83f79b13
kihyuk5566 <kihyuk5566@naver.com>
2026-03-29 01:07:20 +0900

    [BE]fix : 카드 결제 내역 필터링

--- naeda/src/main/java/com/ssafy/naeda/domain/pay/repository/PayTransactionRepository.java
+++ naeda/src/main/java/com/ssafy/naeda/domain/pay/repository/PayTransactionRepository.java
@@ -42,4 +42,21 @@ public interface PayTransactionRepository extends JpaRepository<PayTransaction, 
             @Param("since") LocalDateTime since);
 
     void deleteByUserNo(Long userNo);
+
+    /**
+     * 특정 카드번호로 결제된 트랜잭션의 ssafyTransactionId 목록 조회.
+     * pay_transaction → payment_method → credit_card/debit_card 를 조인하여
+     * 해당 카드로 결제한 건만 필터링한다.
+     */
+    @Query("""
+        SELECT pt.ssafyTransactionId FROM PayTransaction pt
+        WHERE pt.ssafyTransactionId IS NOT NULL
+          AND pt.status = 'SUCCESS'
+          AND pt.paymentMethodId IN (
+              SELECT pm.paymentMethodId FROM PayMethod pm
+              WHERE pm.creditCardId IN (SELECT cc.creditCardId FROM CreditCard cc WHERE cc.cardNo = :cardNo)
+                 OR pm.debitCardId IN (SELECT dc.debitCardId FROM DebitCard dc WHERE dc.cardNo = :cardNo)
+          )
+        """)
+    List<String> findSsafyTransactionIdsByCardNo(@Param("cardNo") String cardNo);
 }
